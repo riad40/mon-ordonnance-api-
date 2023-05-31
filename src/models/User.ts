b3bf4e7537da016cfee8117463290a61c9858215
riad40 <riadabdelaziz40@gmail.com>
2023-05-31 01:29:23 +0100

    updated model

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,39 +3,44 @@ import { User } from "../@types/models/userTypes"
 
 class UserSchema extends mongoose.Schema {
     constructor() {
-        super({
-            fullName: {
-                type: String,
-                required: true,
+        super(
+            {
+                fullName: {
+                    type: String,
+                    required: true,
+                },
+                email: {
+                    type: String,
+                    required: true,
+                },
+                password: {
+                    type: String,
+                    required: true,
+                },
+                avatar: {
+                    type: String,
+                },
+                speciality: {
+                    type: String,
+                    required: true,
+                },
+                dateOfBirth: {
+                    type: String,
+                    required: true,
+                },
+                inpe: {
+                    type: String,
+                    required: true,
+                },
+                phone: {
+                    type: String,
+                    required: true,
+                },
             },
-            email: {
-                type: String,
-                required: true,
+            {
+                timestamps: true,
             },
-            password: {
-                type: String,
-                required: true,
-            },
-            avatar: {
-                type: String,
-            },
-            speciality: {
-                type: String,
-                required: true,
-            },
-            dateOfBirth: {
-                type: String,
-                required: true,
-            },
-            inpe: {
-                type: String,
-                required: true,
-            },
-            phone: {
-                type: String,
-                required: true,
-            },
-        })
+        )
     }
 }
 
